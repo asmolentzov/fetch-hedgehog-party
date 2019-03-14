2fdce4206258d72161e272589e6159ea179df859
Anna Smolentzov <annasmolentzov@gmail.com>
2019-03-14 10:25:40 -0600

    Adds annotation and post feature

--- public/index.js
+++ public/index.js
@@ -1,18 +1,25 @@
 const getHedgehogs = () => {
   $('#hedgehog-info').html('');
+  $('form')[0].reset();
 
+// Sets up the fetch request to the invites endpoint of the hedgehog API, aka, the best API.
   fetch(`https://hedgehog-party.herokuapp.com/api/v1/invites`)
+  // When the fetch Promise resolves, take the response and convert it into JSON
     .then(response => response.json())
+    // When the response.json() Promise resolves, take THAT response as hedgehogs and call appendHedgehogs, passing it the hedgehogs JSON.
     .then(hedgehogs => appendHedgehogs(hedgehogs))
+    // If either of the the Promises rejects, catch the error and log it. 
     .catch(error => console.error({ error }));
 };
 
+// Loops through the hedgehogs array and calls appendHedgehog for each individual hedgie
 const appendHedgehogs = (hedgehogs) => {
   hedgehogs.forEach(hedgehog => {
     appendHedgehog(hedgehog);
   });
 };
 
+// Actually updates the document with a new line item for the hedgehog object. 
 const appendHedgehog = (hedgehog) => {
   $('#invited-hedgehogs-info').append(`
     <article class="invited-hedgehog">
@@ -29,8 +36,23 @@ const appendHedgehog = (hedgehog) => {
   `);
 };
 
-const addNewHedgehog = () => {
-  console.log("we are in the addNewHedgehog function");
+const addNewHedgehog = (event) => {
+  event.preventDefault();
+  const name = document.getElementById('name').value;
+  const hoglets = document.getElementById('hoglets').value;
+  const allergies = document.getElementById('allergies').value;
+  fetch('https://hedgehog-party.herokuapp.com/api/v1/invites', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      name: name,
+      hoglets: hoglets,
+      allergies: allergies
+    }),
+  })
+    .then(response => response.json())
+    .then(idInfo => getHedgehogs())
+    .catch(error => console.error({ error }));
 };
 
 const unInviteHedgehog = () => {
